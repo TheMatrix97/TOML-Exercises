73cf05486078b031717a44b885521c27461c392a
Marc Catrisse <marc@catrisse.net>
2021-03-28 12:58:56 +0200

    ex3 and 4 working

--- ex3.py
+++ ex3.py
@@ -1,20 +1,26 @@
-from cvxopt import solvers, matrix, spdiag, log
 import numpy as np
+from cvxpy import Variable, quad_form, Minimize, Problem
 from scipy.optimize import minimize
 
-#https://courses.csail.mit.edu/6.867/wiki/images/a/a7/Qp-cvxopt.pdf
 
 def objective(x):
     # Objective function
     return pow(x[0], 2) + pow(x[1], 2)
 
 
+def objective_jacob(x):  # TODO REVISAR!
+    # Objective function
+    dx = 2 * x[0]
+    dy = 2 * x[1]
+    return np.array((dx, dy))
+
+
 def ineq_const():
     # I'm negating funs because minimize expects x >= 0
 
     return {'type': 'ineq',
-            'fun': lambda x: np.array([-(pow(x[0], 2) + x[0]*x[1] + pow(x[1], 2) - 3),
-                                       (3*x[0] + 2*x[1] - 3)])
+            'fun': lambda x: np.array([-(pow(x[0], 2) + x[0] * x[1] + pow(x[1], 2) - 3),
+                                       (3 * x[0] + 2 * x[1] - 3)])
             }
 
 
@@ -24,29 +30,45 @@ def run_minimize(x0):
                     )
 
 
-# Ni idea de como convertirlo a cvx... no es un QCQP x1*x2?
-# https://github.com/cvxopt/cvxopt/issues/81
-"""""
-def F(x=None, z=None):
-    if x is None: return 0, matrix(1.0, (n, 1))
-    if min(x) <= 0.0: return None
-    f = -sum(log(x))
-    Df = -(x ** -1).T
-    if z is None: return f, Df
-    H = spdiag(z[0] * x ** -2)
-    return f, Df, H
-"""""
+def run_minimize_jacob(x0):
+    return minimize(objective, x0, method='SLSQP', jac=objective_jacob, constraints=[ineq_const()])
+
+def solve_cvxpy():
+    # Create two scalar optimization variables.
+    x = Variable(2, name='x')
+
+    # Constraints
+    P1 = np.array(np.mat('1. 0.5; 0.5 1.'))
+    f1 = quad_form(x, P1)
+    f2 = 3. * x[0] + 2. * x[1]
+    constraints = [f1 <= 3., f2 >= 3.]
+
+    # Form objective.
+    P0 = np.array(np.mat('1. 0.; 0. 1.'))
+    f0 = quad_form(x, P0)
+    obj = Minimize(f0)
+
+    prob = Problem(obj, constraints)
+    print("solve", prob.solve())  # Returns the optimal value.
+    print("status:", prob.status)
+    print("optimal value p* = ", prob.value)
+    print("optimal var: x1 = ", x[0].value, " x2 = ", x[1].value)
+    print("optimal dual variables lanbda1 = ", constraints[0].dual_value)
+    print("optimal dual variables lanbda2 = ", constraints[1].dual_value)
 
 
 
-#Why jacobian is giving more iterations??
 def main():
     # Initial points x0
     x0_points = [[0, 1], [6, 10]]
     for point in x0_points:
         print("X0 -> " + str(np.array(point)))
-        res = run_minimize(np.array(point))
+        res = run_minimize_jacob(np.array(point))
         print(res)
         print("---------------------------------\n")
+    #CVXPY
+    print("---------CVXPY--------")
+    solve_cvxpy()
+
 
 main()
--- ex4.py
+++ ex4.py
@@ -1,12 +1,31 @@
-from cvxopt import solvers, matrix, spdiag, log
 import numpy as np
-from scipy.optimize import minimize
+from cvxpy import Variable, Minimize, Problem, quad_form, Constant
+# https://www.cvxpy.org/examples/basic/quadratic_program.html
 
-#QCQP problem https://www.youtube.com/watch?v=cWbqYueIdLU&t=2080s
-# https://github.com/cvxopt/cvxopt/issues/81
+
+def solve_cvxpy():
+    # Create one scalar optimization variables.
+    x = Variable(1, name='x')
+
+    # Constraints
+    P1 = np.array(np.mat('1.'))
+    #f1 = quad_form(x, P1)
+    f1 = quad_form(x, P1) - 6*x[0] + Constant(8)
+    constraints = [f1 <= 0.]
+
+    # Form objective.
+    f0 = quad_form(x, P1) + Constant(1)
+    obj = Minimize(f0)
+
+    prob = Problem(obj, constraints)
+    print("solve", prob.solve())  # Returns the optimal value.
+    print("status:", prob.status)
+    print("optimal value p* = ", prob.value)
+    print("optimal var: x1 = ", x[0].value)
+    print("optimal dual variables lanbda1 = ", constraints[0].dual_value)
 
 def main():
-    print("hello")
+    solve_cvxpy()
 
 
 
